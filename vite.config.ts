15f98fa7bef43933a1c14edb72f8097c5b9bfe06
Artem Shinkaruk <artasend@gmail.com>
2022-12-03 15:28:37 +0700

    Configure typescript paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  resolve: {
+    alias: {
+      '~': resolve(__dirname, 'src'),
+    },
+  },
 });
