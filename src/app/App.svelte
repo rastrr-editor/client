13753012e37634d6c22c1f0f1a385e5f4e87a2fb
Artem Shinkaruk <artasend@gmail.com>
2022-12-03 13:08:56 +0700

    Init

---
+++ src/app/App.svelte
@@ -0,0 +1,19 @@
+<main>
+  <h1>Hello this is Rastrr!</h1>
+  <!-- TODO: include home page component -->
+</main>
+
+<style lang="scss">
+  main {
+    height: 100vh;
+    width: 100vw;
+    background: $bg-main;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  h1 {
+    color: $body-color;
+  }
+</style>
