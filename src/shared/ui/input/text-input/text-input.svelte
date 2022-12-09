6cfb38b9f95deada9f4fcf2df79829dc9f237e2a
Дмитрий Лебедко <Lebedko.D.Y@sber-tech.com>
2022-12-09 20:01:21 +0300

    Added TextInput component implementation

---
+++ src/shared/ui/input/text-input/text-input.svelte
@@ -0,0 +1,106 @@
+<script lang="ts">
+  import type { TextFieldType } from './text-input.types';
+
+  interface $$Props extends svelte.JSX.HTMLAttributes<HTMLInputElement> {
+    label?: string;
+    type?: TextFieldType;
+    value?: string | number;
+    disabled?: boolean | undefined;
+    densed?: boolean | undefined;
+    noBorder?: boolean | undefined;
+  }
+
+  export let label: string = '';
+  export let type: TextFieldType = 'text';
+  export let value: string | number = '';
+  export let disabled: boolean | undefined = undefined;
+  export let densed: boolean | undefined = undefined;
+  export let noBorder: boolean | undefined = undefined;
+
+  const defineInputType = (
+    node: HTMLInputElement,
+    inputType: TextFieldType
+  ) => {
+    node.type = inputType;
+  };
+</script>
+
+<label class="container" class:disabled>
+  {#if label.length > 0}
+    <span class="label">{label}</span>
+  {/if}
+
+  <div class="wrapper" class:noBorder>
+    <slot props={{ ...$$restProps, disabled }}>
+      <input
+        {...$$restProps}
+        {disabled}
+        class:densed
+        use:defineInputType={type}
+        bind:value
+        on:input
+        on:change
+        on:focus
+        on:blur
+        on:keydown
+        on:keyup
+        on:keypress
+        on:click
+        on:mouseenter
+        on:mouseover
+        on:mouseleave
+        on:paste
+        on:copy
+      />
+    </slot>
+  </div>
+</label>
+
+<style lang="scss">
+  .container {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .label {
+    margin-bottom: spacing($index: 2);
+    font-size: 1rem;
+    color: $body-color;
+  }
+
+  .wrapper {
+    display: flex;
+    align-items: center;
+    border: 1px solid $border-color;
+    border-radius: $border-radius;
+    background-color: $bg-extra;
+
+    &:hover {
+      border-color: $input-hover-color;
+    }
+
+    &:focus-within {
+      border-color: $input-active-color;
+    }
+  }
+
+  input {
+    @include style-input-field;
+  }
+
+  .disabled {
+    opacity: 0.5;
+
+    .wrapper {
+      border-color: $border-color;
+    }
+  }
+
+  .densed {
+    padding: spacing($index: 1);
+  }
+
+  .noBorder {
+    border-color: transparent;
+  }
+</style>
---
+++ src/shared/ui/input/text-input/text-input.types.ts
@@ -0,0 +1,1 @@
+export type TextFieldType = 'text' | 'email' | 'password';
