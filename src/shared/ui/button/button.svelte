2da029ab050b182ddb55ac716c1a73217e98454b
Дмитрий Лебедко <Lebedko.D.Y@sber-tech.com>
2022-12-09 20:00:49 +0300

    Added Button component implementation

---
+++ src/shared/ui/button/button.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+  import type { ButtonType } from './button.types';
+
+  interface $$Props extends svelte.JSX.HTMLAttributes<HTMLButtonElement> {
+    type?: ButtonType;
+    disabled?: boolean | undefined;
+  }
+
+  export let type: ButtonType = 'button';
+  export let disabled: boolean | undefined = undefined;
+</script>
+
+<button
+  {...$$restProps}
+  {type}
+  {disabled}
+  on:keyup
+  on:keydown
+  on:keypress
+  on:focus
+  on:blur
+  on:click
+  on:mouseover
+  on:mouseenter
+  on:mouseleave
+>
+  <slot />
+</button>
+
+<style lang="scss">
+  button {
+    @include reset-button($resetFont: false);
+    @include action-cursor;
+    min-width: 190px;
+    padding: spacing(3) spacing(5);
+    border-radius: $border-radius;
+    background-color: $body-color;
+    font-size: 1rem;
+    font-weight: 500;
+    color: $bg-extra;
+    transition: background-color $animation-time;
+
+    &:hover,
+    &:focus {
+      background-color: $button-hover-color;
+    }
+
+    &:focus {
+      outline: none;
+    }
+
+    &:active {
+      background-color: $button-active-color;
+    }
+
+    &:disabled {
+      color: $button-disabled-color;
+      cursor: url('~/shared/ui/icons/cursor-default.svg'), auto;
+
+      &,
+      &:hover {
+        background-color: $button-disabled-bg-color;
+      }
+    }
+  }
+</style>
