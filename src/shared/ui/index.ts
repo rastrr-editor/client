c4bd458aaa552b9fb2966e8899afd90910a938c4
Дмитрий Лебедко <Lebedko.D.Y@sber-tech.com>
2022-12-10 23:45:47 +0300

    Updated shared/ui public api file

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,4 +1,7 @@
 export { Button } from './button';
 export { Checkbox } from './checkbox';
-export { TextInput, NumberInput } from './input/';
+export { IconButton } from './icon-button';
+export { TextInput, NumberInput } from './input';
 export { Range } from './range';
+export { Search } from './search';
+
