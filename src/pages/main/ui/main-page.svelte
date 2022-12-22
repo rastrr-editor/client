f5db97646512a3b68e5e2aa10f27d1a749855c66
Artem Shinkaruk <artasend@gmail.com>
2022-12-22 23:12:42 +0300

    Integrate canvas container component into main page

--- src/pages/main/ui/main-page.svelte
+++ src/pages/main/ui/main-page.svelte
@@ -1,17 +1,11 @@
 <script lang="ts">
   import { CreateProject } from '~/features/create-project';
+  import MainPageCanvasContainer from './main-page-canvas-container.svelte';
   import MainPageHeader from './main-page-header.svelte';
 
   let showNewProject = false;
 </script>
 
 <MainPageHeader on:createNewProject={() => (showNewProject = true)} />
-<main id="canvas-container" />
+<MainPageCanvasContainer />
 <CreateProject bind:open={showNewProject} />
-
-<style lang="scss">
-  #canvas-container {
-    background-color: $bg-canvas;
-    height: calc(100vh - 1.75rem);
-  }
-</style>
