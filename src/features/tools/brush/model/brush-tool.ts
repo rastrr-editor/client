cc5c00d23976463f2a76dde4ff2998511f251a01
Artem Shinkaruk <artasend@gmail.com>
2022-12-24 23:42:15 +0300

    Fix brush tool cursor

--- src/features/tools/brush/model/brush-tool.ts
+++ src/features/tools/brush/model/brush-tool.ts
@@ -13,16 +13,21 @@ export default class BrushTool implements Tool<BrushOptions> {
 
   getCursor(): string {
     return `url("data:image/svg+xml;base64,${btoa(
-      `<svg width="${this.#options.size + 1}" height="${
-        this.#options.size + 1
-      }" viewBox="0 0 ${this.#options.size + 1} ${
-        this.#options.size + 1
-      }" xmlns="http://www.w3.org/2000/svg"><circle cx="${
-        this.#options.size / 2 + 0.5
-      }" cy="${this.#options.size / 2 + 0.5}" r="${
-        this.#options.size / 2
-      }" fill="none" stroke="#000"/></svg>`
-    )}")`;
+      `<svg
+        width="${this.#options.size + 2}"
+        height="${this.#options.size + 2}"
+        viewBox="0 0 ${this.#options.size + 2} ${this.#options.size + 2}"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle
+          cx="${this.#options.size / 2 + 1}"
+          cy="${this.#options.size / 2 + 1}"
+          r="${this.#options.size / 2}"
+          fill="none"
+          stroke="#000"
+        />
+      </svg>`
+    )}") ${this.#options.size / 2 + 1} ${this.#options.size / 2 + 1}`;
   }
 
   setOptions(options: BrushOptions): void {
