86ffca9e931b968214568ea5c728a7f597fd47e3
Artem Shinkaruk <artasend@gmail.com>
2022-12-24 11:12:19 +0300

    Implement tool hover

--- src/widgets/tool-panel/ui/tool-panel.svelte
+++ src/widgets/tool-panel/ui/tool-panel.svelte
@@ -48,7 +48,30 @@
   }
 
   button {
-    @include reset-button(false);
+    @include reset-button(true);
     @include action-cursor;
+    position: relative;
+
+    &::after {
+      box-sizing: content-box;
+      content: '';
+      z-index: -1;
+      position: absolute;
+      top: -2px;
+      left: -3px;
+      width: 100%;
+      height: 100%;
+      border-radius: 2px;
+      padding: 0.125rem 0.3125rem 0.375rem 0.1875rem;
+    }
+
+    &:hover::after {
+      background-color: $bg-extra;
+    }
+
+    > :global(svg) {
+      font-size: 1.5rem;
+      color: #fff;
+    }
   }
 </style>
