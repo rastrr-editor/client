a43e650033f1ede561fc481df666158780e67c1f
Artem Shinkaruk <artasend@gmail.com>
2022-12-29 19:22:45 +0700

    Add tool help tooltip component

--- src/entities/tool/index.ts
+++ src/entities/tool/index.ts
@@ -1,3 +1,4 @@
 export * from './types';
 export * from './model';
 export { default as ToolOptionsTooltip } from './ui/tool-options-tooltip.svelte';
+export { default as ToolHelpTooltip } from './ui/tool-help-tooltip.svelte';
