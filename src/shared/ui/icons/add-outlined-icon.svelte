f618ed4461415dce43cdc175fd91755018a161f8
Dmitry Lebedko <Dimeliora710@gmail.com>
2022-12-31 15:20:30 +0300

    Updated icons

--- src/shared/ui/icons/add-outlined-icon.svelte
+++ src/shared/ui/icons/add-outlined-icon.svelte
@@ -8,6 +8,7 @@
   viewBox="0 0 24 24"
   fill="none"
   stroke="currentColor"
+  stroke-width="1"
   xmlns="http://www.w3.org/2000/svg"
   {...$$restProps}
 >
@@ -19,18 +20,7 @@
     rx="1.5"
     stroke-dasharray="2 2"
     d="M2 0.5H22A1.5 1.5 0 0 1 23.5 2V22A1.5 1.5 0 0 1 22 23.5H2A1.5 1.5 0 0 1 0.5 22V2A1.5 1.5 0 0 1 2 0.5z"
-    stroke-width="1"
   />
-  <path
-    d="M12 9.083V14.917"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-    stroke-width="1"
-  />
-  <path
-    d="M9.084 12H14.917"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-    stroke-width="1"
-  />
+  <path d="M12 9.083V14.917" stroke-linecap="round" stroke-linejoin="round" />
+  <path d="M9.084 12H14.917" stroke-linecap="round" stroke-linejoin="round" />
 </svg>
