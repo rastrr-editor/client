2aa026431dfdebe0ba5bb088ff0c9974ef2f1d11
Artem Shinkaruk <artasend@gmail.com>
2023-01-03 19:32:07 +0700

    Export repository from project entity

--- src/entities/project/model/index.ts
+++ src/entities/project/model/index.ts
@@ -1,1 +1,11 @@
+import {
+  IndexedDBProjectRepository,
+  type ProjectRepository,
+} from './repository';
+
 export * as projectStore from './store';
+export { IndexedDBProjectRepository, type ProjectRepository };
+
+export function createProjectRepository(): ProjectRepository {
+  return new IndexedDBProjectRepository();
+}
