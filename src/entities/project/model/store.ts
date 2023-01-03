95129ed2abfb7153a4935e49bc43f46e39f90d6a
Artem Shinkaruk <artasend@gmail.com>
2023-01-03 19:33:42 +0700

    Store project using repository on project create

--- src/entities/project/model/store.ts
+++ src/entities/project/model/store.ts
@@ -1,4 +1,4 @@
 import { writable } from 'svelte/store';
-import type { Project } from '../types';
+import type { Project } from '~/shared/api';
 
 export const activeProject = writable<Project | null>(null);
