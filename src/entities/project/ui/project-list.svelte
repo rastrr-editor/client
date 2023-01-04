912ee66009c8aae2aaa96516d86cb6e9f399c696
Artem Shinkaruk <artasend@gmail.com>
2023-01-04 19:50:07 +0700

    Implement load more for project list

--- src/entities/project/ui/project-list.svelte
+++ src/entities/project/ui/project-list.svelte
@@ -3,44 +3,90 @@
   import { DotFlashing } from '~/shared/ui';
   import { createProjectRepository } from '../model';
   import ProjectCard from './project-card.svelte';
+  import type { Project } from '~/shared/api';
 
   export let open: boolean = false;
 
+  const unexpectedError = 'Не удалось загрузить данные, попробуйте ещё раз!';
   const repository = createProjectRepository();
-  const page = 1;
+  const scrollToEndDelta = 10;
+
+  let content: HTMLDivElement;
+  let page = 1;
+  let error = '';
+  let pageLoading = false;
+  let items: Project[] = [];
+  let totalPages: number = 0;
   let center = true;
-  let dataLoading: ReturnType<typeof repository.paginate> | Promise<null> =
+  let firstLoad: ReturnType<typeof repository.paginate> | Promise<null> =
     Promise.resolve(null);
 
   $: {
     center = true;
-    dataLoading = open ? repository.paginate({ page }) : Promise.resolve(null);
-    dataLoading.then((result) => {
+    page = 1;
+    firstLoad = open ? repository.paginate({ page: 1 }) : Promise.resolve(null);
+    firstLoad.then((result) => {
       if (result && result.total > 0) {
         center = false;
+        items = Array.from(result.items);
+        totalPages = result.total;
       }
     });
+  }
+
+  function loadMore() {
+    if (
+      !pageLoading &&
+      page < totalPages &&
+      content.offsetHeight + content.scrollTop >
+        content.scrollHeight - scrollToEndDelta
+    ) {
+      pageLoading = true;
+      repository
+        .paginate({ page: ++page })
+        .then((result) => {
+          error = '';
+          items = items.concat(Array.from(result.items));
+        })
+        .catch(() => {
+          error = unexpectedError;
+        })
+        .finally(() => {
+          pageLoading = false;
+        });
+    }
   }
 </script>
 
 <Modal size="large" class="project-list" bind:open>
   <h2>Проекты</h2>
-  <div class="content" class:center>
-    {#await dataLoading}
+  <div bind:this={content} on:scroll={loadMore} class="content" class:center>
+    {#await firstLoad}
       <DotFlashing />
-    {:then result}
-      {#if result != null && result.total > 0}
+    {:then _}
+      {#if items.length > 0}
         <div class="list">
-          {#each Array.from(result.items) as project (project.id)}
+          {#each items as project (project.id)}
             <ProjectCard {project} />
           {/each}
         </div>
+        {#if pageLoading}
+          <div class="loading-more">
+            <DotFlashing />
+          </div>
+        {:else if error}
+          <div class="loading-more">
+            <p>{error}</p>
+          </div>
+        {/if}
       {:else}
         <div class="empty">
           <p>У вас еще нет проектов</p>
           <p>Сохраните свой первый проект и он появится тут</p>
         </div>
       {/if}
+    {:catch}
+      <p>{unexpectedError}</p>
     {/await}
   </div>
 </Modal>
@@ -58,7 +104,7 @@
 
   .content {
     background-color: $bg-extra;
-    height: spacing(125);
+    height: spacing(134);
     @include custom-scroll;
     overflow-x: hidden;
 
@@ -68,6 +114,13 @@
       align-items: center;
       justify-content: center;
     }
+  }
+
+  .loading-more {
+    padding-top: spacing(14);
+    padding-bottom: spacing(22);
+    display: flex;
+    justify-content: center;
   }
 
   .empty {
