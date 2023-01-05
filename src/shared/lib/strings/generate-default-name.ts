4d0f256b9672ba7b310ca78eca42a75a31ee1a71
Dmitry Lebedko <Dimeliora710@gmail.com>
2023-01-05 11:42:02 +0300

    Added lib function for default name generation

---
+++ src/shared/lib/strings/generate-default-name.ts
@@ -0,0 +1,22 @@
+/**
+ * Function generates a string based on param defaultName.
+ * If value of defaultName is existing in existingNames array
+ * function recursively produces defaultName value with numeric postfix
+ * @param existingNames Array of existing names
+ * @param defaultName Default name template
+ * @returns Generated string based on defaultName with possible numeric postfix
+ */
+export default function getDefaultName(
+  existingNames: string[],
+  defaultName: string
+): string {
+  function iterate(name: string, postfix: number = 0): string {
+    const newName = postfix === 0 ? name : `${name} ${postfix}`;
+
+    return existingNames.includes(newName)
+      ? iterate(name, postfix + 1)
+      : newName;
+  }
+
+  return iterate(defaultName);
+}
