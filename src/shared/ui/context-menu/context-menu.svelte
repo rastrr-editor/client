b0b8708cc80373683fbcf11888eb9a19c8be979d
Dmitry Lebedko <Dimeliora710@gmail.com>
2023-01-05 11:42:39 +0300

    Implemented ContextMenu component

---
+++ src/shared/ui/context-menu/context-menu.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+  import { clickOutside } from '~/shared/lib/actions';
+
+  export let open: boolean = false;
+  export let top: number = -9999;
+  export let left: number = -9999;
+
+  const dispatch = createEventDispatcher();
+
+  function hideContextMenu(): void {
+    open = false;
+
+    dispatch('hide');
+  }
+
+  function hideContextMenuByKeydown(event: KeyboardEvent): void {
+    if (event.key === 'Escape' || event.code === 'Escape') {
+      open = false;
+    }
+  }
+</script>
+
+<div
+  role="menu"
+  class="context-menu"
+  class:open
+  style:top={`${top}px`}
+  style:left={`${left}px`}
+  use:clickOutside={{ callback: hideContextMenu }}
+  on:keydown={hideContextMenuByKeydown}
+>
+  <slot />
+</div>
+
+<style lang="scss">
+  .context-menu {
+    @include typography(body2);
+
+    position: fixed;
+    z-index: 100;
+    display: flex;
+    flex-direction: column;
+    padding: spacing(0.5);
+    visibility: hidden;
+    border: 1px solid $modal-border-color;
+    border-radius: $border-radius;
+    background-color: var(--context-menu-background-color, #{$bg-extra});
+  }
+
+  .open {
+    visibility: visible;
+  }
+</style>
