66d779b478d58107bdfcda4ee3cf9d093176319d
Artem Shinkaruk <artasend@gmail.com>
2023-01-13 01:12:24 +0700

    Integrate history dock panel

--- src/pages/main/ui/main-page-dock-panel.svelte
+++ src/pages/main/ui/main-page-dock-panel.svelte
@@ -2,6 +2,7 @@
   import { chooseColorStore } from '~/features/tools/choose-color';
   import { PalettesDockPanel } from '~/features/dock-panels/palettes';
   import { LayersDockPanel } from '~/features/dock-panels/layers';
+  import { HistoryDockPanel } from '~/features/dock-panels/history';
   import { viewport } from '../model/store';
 
   const { mainColor, secondaryColor } = chooseColorStore;
@@ -13,11 +14,15 @@
   <LayersDockPanel
     layerList={$viewport?.layers}
     imageSize={$viewport?.options.imageSize} />
+  <HistoryDockPanel viewport={$viewport} />
 </aside>
 
 <style lang="scss">
   aside {
+    height: 100vh;
     border-left: 1px solid $border-color;
     background-color: $bg-extra;
+    @include custom-scroll;
+    overflow-x: hidden;
   }
 </style>
