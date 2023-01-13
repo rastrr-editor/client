2cf34b3a84f6202cd96116c42cc45b3c8b990d8f
Dmitry Lebedko <Dimeliora710@gmail.com>
2023-01-13 08:53:03 +0300

    Added types for tool-panel widget

---
+++ src/widgets/tool-panel/types.ts
@@ -0,0 +1,22 @@
+import type { SvelteComponent } from 'svelte';
+
+import type { ShapeTool } from '~/entities/tool';
+
+type ShapeToolConstructor = new () => ShapeTool;
+
+type ToolConstants = {
+  id: string;
+  name: string;
+  hotkey: string | null;
+};
+
+export type ShapeToolData = {
+  icon: typeof SvelteComponent;
+  tool: ShapeToolConstructor;
+  constants: ToolConstants;
+};
+
+export type ToolTooltip = {
+  show: boolean;
+  trigger: HTMLButtonElement | null;
+};
