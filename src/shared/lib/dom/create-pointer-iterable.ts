dbb2b421791f466803701482ce828826db48f16e
Artem Shinkaruk <artasend@gmail.com>
2023-01-14 12:43:41 +0700

    Fix iterable points fluctuations

--- src/shared/lib/dom/create-pointer-iterable.ts
+++ src/shared/lib/dom/create-pointer-iterable.ts
@@ -7,34 +7,50 @@ export default function createPointerIterable(
   offset: Rastrr.Point = { x: 0, y: 0 }
 ): AsyncIterable<Rastrr.Point> {
   const coords = getCoords(boundingContainer);
+  // NOTE: it's a hack. It would be better to have points in iterable as pairs [prev, curr]
+  let prevPoint: Rastrr.Point | null = null;
   // TODO: refactor using dot notation
-  return asyncIter.map(
-    asyncIter.every(
-      asyncIter.seq(
-        asyncIter.toAsyncIter([initialEvent]),
-        asyncIter.until(
-          asyncIter.any(
-            events.on(document.body, 'pointermove'),
-            events.on(document.body, 'pointerup')
-          ),
-          events.onlyEvent('pointerup')
-        )
+  return asyncIter.filter(
+    asyncIter.map(
+      asyncIter.every(
+        asyncIter.seq(
+          asyncIter.toAsyncIter([initialEvent]),
+          asyncIter.until(
+            asyncIter.any(
+              events.on(document.body, 'pointermove'),
+              events.on(document.body, 'pointerup')
+            ),
+            events.onlyEvent('pointerup')
+          )
+        ),
+        (e) => {
+          return !(
+            e.pageX < coords.left ||
+            e.pageX > coords.right ||
+            e.pageY < coords.top ||
+            e.pageY > coords.bottom
+          );
+        }
       ),
       (e) => {
-        // TODO: it's better to use offsetX, offsetY
-        // because it prevents cursor jumps when user reaches the container border,
-        // but there are problems when canvas container has scroll
-        return !(
-          e.pageX < coords.left ||
-          e.pageX > coords.right ||
-          e.pageY < coords.top ||
-          e.pageY > coords.bottom
-        );
+        const point = {
+          x: e.offsetX - offset.x,
+          y: e.offsetY - offset.y,
+        };
+        return point;
       }
     ),
-    (e) => ({
-      x: e.offsetX - offset.x,
-      y: e.offsetY - offset.y,
-    })
+    (point) => {
+      // Ignore fluctuations to prevent cursor jumps when user reaches the container border
+      // NOTE: a * b < 0 means that "a" and "b" have different signs
+      if (
+        prevPoint != null &&
+        (prevPoint.y * point.y < 0 || prevPoint.x * point.x < 0)
+      ) {
+        return false;
+      }
+      prevPoint = point;
+      return true;
+    }
   );
 }
