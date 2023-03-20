a9a68f8430f4b62a65ee86b0df9944c08a8c2314
Artem Shinkaruk <artasend@gmail.com>
2023-03-20 23:37:12 +0700

    Use download blob in export image

--- src/pages/main/model/export-image.ts
+++ src/pages/main/model/export-image.ts
@@ -1,4 +1,5 @@
 import { get } from 'svelte/store';
+import { downloadBlob } from '~/shared/lib/url';
 import { viewport as viewportStore } from './store';
 
 const IMAGE_MIME_TYPE = {
@@ -17,25 +18,8 @@ export default function exportImage(name: string, ext: 'png' | 'jpg'): void {
       if (blob == null) {
         throw new Error('No blob');
       }
-      // NOTE: consider using browser.downloads API if possible
-      const dataUrl = URL.createObjectURL(blob);
       const fileName = `${name}.${ext}`;
-      // Simulate link click to download the image
-      const anchor = document.createElement('a');
-      anchor.setAttribute('href', dataUrl);
-      anchor.setAttribute('target', '_blank');
-      anchor.setAttribute('download', fileName);
-      if (typeof document.createEvent === 'function') {
-        const event = document.createEvent('MouseEvents');
-        event.initEvent('click', true, true);
-        anchor.dispatchEvent(event);
-      } else if (typeof anchor.click === 'function') {
-        anchor.click();
-      }
-      // 10 seconds should be enough to download the image
-      setTimeout(() => {
-        URL.revokeObjectURL(dataUrl);
-      }, 10000);
+      downloadBlob(blob, fileName);
     })
     .catch((error) => {
       console.error(error);
