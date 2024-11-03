133ea50d701b6a789d6013284851664cc5df894a
Artem Shinkaruk <artasend@gmail.com>
2024-11-03 13:11:15 +0200

    Disable warning for slow builds

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,19 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte({
+      onwarn(warning, defaultHandler) {
+        if (
+          warning.message.indexOf(
+            'depends on more than 10 external files which can cause slow builds and poor DX',
+          ) === -1
+        ) {
+          defaultHandler(warning);
+        }
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '~': resolve(__dirname, 'src'),
