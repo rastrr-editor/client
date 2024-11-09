2cbd3063d2987a725c484d393a40c17ccb4ff110
Artem Shinkaruk <artasend@gmail.com>
2024-11-09 22:43:25 +0200

    Implement Ctrl+S hotkey

--- src/pages/main/model/store.ts
+++ src/pages/main/model/store.ts
@@ -1,8 +1,11 @@
 import type { Viewport } from '@rastrr-editor/core';
 import { location } from 'svelte-spa-router';
 import { writable } from 'svelte/store';
+
 import { hotkeys } from '~/features/hotkeys-manager';
+
 import { handleHistoryHotkeys, HistoryEvents } from './handle-history-hotkeys';
+import saveProject from './save-project';
 
 export const viewport = writable<Viewport | null>(null);
 
@@ -15,6 +18,16 @@ location.subscribe((value) => {
   openAbout.set(value === '/about');
 });
 
-hotkeys.register(['control', 'z'], HistoryEvents.UNDO);
-hotkeys.register(['control', 'shift', 'z'], HistoryEvents.REDO);
+const control = /Mac/i.test(navigator.platform) ? 'meta' : 'control';
+
+hotkeys.register([control, 'z'], HistoryEvents.UNDO);
+hotkeys.register([control, 'shift', 'z'], HistoryEvents.REDO);
+
 viewport.subscribe(handleHistoryHotkeys);
+
+hotkeys.register([control, 's'], 'save');
+
+hotkeys.emitter.on('activated:save', (_alias, _ctx, e) => {
+  e.preventDefault();
+  saveProject().then(() => console.log('Saved'));
+});
